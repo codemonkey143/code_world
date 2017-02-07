ff9b74a5e3f61b1af8f6882a4632150954818354
uday <uurouthu@gmail.com>
2017-02-07 04:17:48 +0000

    removed secret key

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,4 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/blogful"
     DEBUG = True
-    SECRET_KEY = os.environ.get("\xf8mZ\xc5\xf47\xbe\xbc\xca\x84}\xa2",os.urandom(12))
+    SECRET_KEY = os.environ.get("SECRET_KEY_HERE",os.urandom(12))
